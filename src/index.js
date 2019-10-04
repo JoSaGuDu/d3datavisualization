01fcbdfb57c8b536bd6d3a4f297c1f5fc666e733
Jose S <reaktechnologiesdsignws@gmail.com>
2019-10-04 17:03:44 -0400

    BAsic shapes added

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-console.log('Ground control to mayor Tom...');
-import _ from 'lodash';
-import * as d3 from 'd3';
+console.log("Ground control to mayor Tom...");
+import _ from "lodash";
+import * as d3 from "d3";
 
-function component() 
-{
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-    d3.select("body")
-    .append("svg")
-    .append("text")
-    .text("Hello 3d")
-    .attr("x", "100")
-    .attr("y", "100")
-    ;
+  let svg = d3.select("body").append("svg");
+  svg
+    .append("circle")
+    .attr("fill", "red")
+    .attr("r", 20)
+    .attr("cx", 100)
+    .attr("cy", 100);
+  svg
+    .append("circle")
+    .attr("fill", "red")
+    .attr("r", 80)
+    .attr("cx", 200)
+    .attr("cy", 100);
+  svg
+    .append("circle")
+    .attr("fill", "red")
+    .attr("r", 10)
+    .attr("cx", 300)
+    .attr("cy", 100);
 
-    return element;
-
-
+  return element;
 }
 
 document.body.appendChild(component());
