37566d90d95c97b0d4fe0c5c77c29d2b5dd683ef
Jose S. G <joseguti5@gmail.com>
2019-10-03 22:26:31 -0400

    D3 and webpack running

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('Ground control to mayor Tom...');
+import _ from 'lodash';
+import * as d3 from 'd3';
+
+function component() 
+{
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    d3.select("body")
+    .append("svg")
+    .append("text")
+    .text("Hello 3d")
+    .attr("x", "100")
+    .attr("y", "100")
+    ;
+
+    return element;
+
+
+}
+
+document.body.appendChild(component());
