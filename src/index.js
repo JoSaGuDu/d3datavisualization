2ce3a5b0ce13f92ce56694b09771fe443e3fffd1
Jose S <reaktechnologiesdsignws@gmail.com>
2019-10-18 15:59:41 -0400

    Transformation and geometry comparation to reflect the real dat meaning

--- src/index.js
+++ src/index.js
@@ -6,25 +6,48 @@ function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  let svg = d3.select("body").append("svg");
+  let svg = d3
+    .select("body")
+    .append("svg")
+    .attr("transform", "scale(8)");
 
-  let current_x = 100;
-  let current_y = 100;
+  let current_x = 455;
+  let current_y = 455;
   function drawCircle(radius) {
     svg
       .append("circle")
       .attr("fill", "red")
-      .attr("r", radius * 10)
+      .attr("r", radius)
       .attr("cx", current_x)
       .attr("cy", current_y);
 
-    current_x += 100;
+    current_x += 25;
   }
 
   drawCircle(2);
   drawCircle(8);
   drawCircle(1);
   drawCircle(2);
+
+  //Working with a geometry that reflects the real translation from data to shape
+  current_x = 455;
+  current_y = 470;
+  function drawRectangle(radius) {
+    svg
+      .append("rect")
+      .attr("fill", "blue")
+      .attr("width", 20)
+      .attr("height", radius)
+      .attr("x", current_x)
+      .attr("y", current_y);
+
+    current_x += 25;
+  }
+
+  drawRectangle(2);
+  drawRectangle(8);
+  drawRectangle(1);
+  drawRectangle(2);
 
   return element;
 }
